0d1929ea1b4774f13e065ec8d3f4a55be0f1850b
rushi444 <rushi44@gmail.com>
2020-06-05 15:40:59 -0700

    params working now, AWS successfully set up

--- backend/graphql.ts
+++ backend/graphql.ts
@@ -3,17 +3,26 @@ const { ApolloServer, gql } = require('apollo-server-lambda');
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    hello($name: String!): String
+    hello(name: String!): String
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: (parent, args, context, info) => `Hello ${args.name}`,
+    hello: (parent, args, context, info) => `Hi, ${args.name}`,
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  }),
+});
 
 exports.graphqlHandler = server.createHandler();

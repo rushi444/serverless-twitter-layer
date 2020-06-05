6a352d3d630c311e7eb67b19ed188a7b59c782e9
rushi444 <rushi44@gmail.com>
2020-06-05 14:59:17 -0700

    optional param

--- backend/graphql.ts
+++ backend/graphql.ts
@@ -2,16 +2,16 @@ const { ApolloServer, gql } = require('apollo-server-lambda');
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
-  type Query {
+  type Query ($name: String!){
     hello: String
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
-    Query: {
-        hello: () => 'Hello world!',
-    },
+  Query: {
+    hello: (parent, args, context, info) => `Hello ${args.name}`,
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
